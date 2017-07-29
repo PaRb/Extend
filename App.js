a4f29d3e8ddf429652314efaeb7f54e33629103a
Pierangelo Rothenbuehler <pierangelooo@gmail.com>
2017-07-29 14:46:55 +0200

    Working react native app, displays two persons in FlatList

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Slayer!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View>
+        <FlatList
+          data={[{ key: "Pierangelo" }, { key: "Florian" }]}
+          renderItem={({ item }) =>
+            <Text style={styles.item}>
+              {item.key}
+            </Text>}
+        />
       </View>
-
-      //FlatList pour liste de contacts (ou Sectionlist)
-      //data qu'on veut: Prénom + Nom, repas, description
     );
   }
 }
@@ -19,8 +20,13 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44
+  }
 });
--- App.test.js
+++ App.test.js
@@ -2,8 +2,20 @@ import React from 'react';
 import App from './App';
 
 import renderer from 'react-test-renderer';
+import { StyleSheet, Text, View } from 'react-native';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const rendered = renderer.create(<App />).toJSON();
-  expect(rendered).toBeTruthy();
-});
+
+describe('App.js', () => {
+
+  it('renders without crashing', () => {
+    const rendered = renderer.create(<App />).toJSON();
+    expect(rendered).toBeTruthy();
+  });
+
+  it('Renders 3 Text', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Text).length).toBe(3);
+  });
+
+})
