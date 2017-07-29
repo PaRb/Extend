1d991d05d574848d29869b0297296708757d03ed
Pierangelo Rothenbuehler <pierangelooo@gmail.com>
2017-07-29 15:40:50 +0200

    Added email, firstName, lastName, repas and id to FlatList

--- App.js
+++ App.js
@@ -6,11 +6,35 @@ export default class App extends React.Component {
     return (
       <View>
         <FlatList
-          data={[{ key: "Pierangelo" }, { key: "Florian" }]}
+          data={[
+            {
+              email: "pierangelooo@gmail.com",
+              firstName: "Pierangelo",
+              lastName: "Rothenbühler",
+              repas: "#11",
+              id: 1
+            },
+            {
+              email: "florian.bienefelt@gmail.com",
+              firstName: "Florian",
+              lastName: "Bienefelt",
+              repas: "#10",
+              id: 2
+            }
+          ]}
+          keyExtractor={(item, index) => item.id}
           renderItem={({ item }) =>
-            <Text style={styles.item}>
-              {item.key}
-            </Text>}
+            <View style={styles.itemView}>
+              <Text style={styles.itemName}>
+                {item.firstName} {item.lastName}
+              </Text>
+              <Text style={styles.itemRepas}>
+                {item.repas}
+              </Text>
+              <Text style={styles.itemEmail}>
+                {item.email}
+              </Text>
+            </View>}
         />
       </View>
     );
@@ -24,9 +48,25 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center"
   },
-  item: {
+  itemName: {
+    padding: 10,
+    fontSize: 20,
+    height: 44
+  },
+  itemEmail: {
     padding: 10,
     fontSize: 18,
-    height: 44
+    height: 44,
+    color: "gray"
+  },
+  itemView: {
+    flex: 1,
+    borderBottomColor: "black",
+    borderBottomWidth: 1
+  },
+  itemRepas: {
+    padding: 10,
+    fontSize: 18,
+    fontWeight: "bold"
   }
 });
--- App.test.js
+++ App.test.js
@@ -1,21 +1,12 @@
-import React from 'react';
-import App from './App';
+import React from "react";
+import App from "./App";
 
-import renderer from 'react-test-renderer';
-import { StyleSheet, Text, View } from 'react-native';
-import { shallow } from 'enzyme';
+import renderer from "react-test-renderer";
+import { StyleSheet, Text, View } from "react-native";
 
-
-describe('App.js', () => {
-
-  it('renders without crashing', () => {
+describe("App.js", () => {
+  it("renders without crashing", () => {
     const rendered = renderer.create(<App />).toJSON();
     expect(rendered).toBeTruthy();
   });
-
-  it('Renders 3 Text', () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.find(Text).length).toBe(3);
-  });
-
-})
+});
