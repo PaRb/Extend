790bb4e6f8e24467bced5d5e6eff3b1479499a79
Pierangelo Rothenbuehler <pierangelooo@gmail.com>
2017-07-29 16:08:14 +0200

    Create separate Contacts.js file to clean App.js

--- App.js
+++ App.js
@@ -1,72 +1,34 @@
 import React from "react";
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import Contacts from "/Users/Pierangelo/Documents/Prog/Extend/Contacts.js";
 
 export default class App extends React.Component {
   render() {
     return (
-      <View>
-        <FlatList
-          data={[
-            {
-              email: "pierangelooo@gmail.com",
-              firstName: "Pierangelo",
-              lastName: "Rothenbühler",
-              repas: "#11",
-              id: 1
-            },
-            {
-              email: "florian.bienefelt@gmail.com",
-              firstName: "Florian",
-              lastName: "Bienefelt",
-              repas: "#10",
-              id: 2
-            }
-          ]}
-          keyExtractor={(item, index) => item.id}
-          renderItem={({ item }) =>
-            <View style={styles.itemView}>
-              <Text style={styles.itemName}>
-                {item.firstName} {item.lastName}
-              </Text>
-              <Text style={styles.itemRepas}>
-                {item.repas}
-              </Text>
-              <Text style={styles.itemEmail}>
-                {item.email}
-              </Text>
-            </View>}
-        />
-      </View>
+      <Contacts
+        users={[
+          {
+            email: "pierangelooo@gmail.com",
+            firstName: "Pierangelo",
+            lastName: "Rothenbühler",
+            repas: "#11",
+            id: 1
+          },
+          {
+            email: "florian.bienefelt@gmail.com",
+            firstName: "Florian",
+            lastName: "Bienefelt",
+            repas: "#10",
+            id: 2
+          },
+          {
+            email: "constace.legay@gmail.com",
+            firstName: "",
+            lastName: "",
+            repas: "",
+            id: 3
+          }
+        ]}
+      />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  itemName: {
-    padding: 10,
-    fontSize: 20,
-    height: 44
-  },
-  itemEmail: {
-    padding: 10,
-    fontSize: 18,
-    height: 44,
-    color: "gray"
-  },
-  itemView: {
-    flex: 1,
-    borderBottomColor: "black",
-    borderBottomWidth: 1
-  },
-  itemRepas: {
-    padding: 10,
-    fontSize: 18,
-    fontWeight: "bold"
-  }
-});
---
+++ Contacts.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+
+export default class Contacts extends React.Component {
+  render() {
+    return (
+      <View>
+        <FlatList
+          data={this.props.users}
+          keyExtractor={(item, index) => item.id}
+          renderItem={({ item }) =>
+            <View style={styles.itemView}>
+              <Text style={styles.itemName}>
+                {item.firstName || "Prénom"} {item.lastName || "Nom"}
+              </Text>
+              <Text style={styles.itemRepas}>
+                {item.repas || "Pas encore assigné"}
+              </Text>
+              <Text style={styles.itemEmail}>
+                {item.email || "Pas encore d'adresse email"}
+              </Text>
+            </View>}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  itemName: {
+    padding: 10,
+    fontSize: 20,
+    height: 44
+  },
+  itemEmail: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+    color: "gray"
+  },
+  itemView: {
+    flex: 1,
+    borderBottomColor: "black",
+    borderBottomWidth: 1
+  },
+  itemRepas: {
+    padding: 10,
+    fontSize: 18,
+    fontWeight: "bold"
+  }
+});
