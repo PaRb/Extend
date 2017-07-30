e885643b0e0c4e19f91efb629c82976b947ac24d
Florian Bienefelt <florian.bienefelt@gmail.com>
2017-07-30 12:32:40 +0200

    cleaned up all api code

---
+++ api/apiHelpers.js
@@ -0,0 +1,57 @@
+import Base64 from './Base64';
+import queryString from 'query-string';
+
+import key from '../mailchimp-key.js';
+
+export const MAIN_LIST_ID = '2530fc93d7';
+export const MEAL_GROUPS_ID = 'aa093740d5';
+
+const baseURL = 'https://us15.api.mailchimp.com/3.0';
+
+export const generateRequest = (
+  path,
+  method = 'GET',
+  query,
+  body = undefined,
+) => {
+  let fullPath = baseURL + path;
+  if (query) {
+    const stringifiedQuery = queryString.stringify(query);
+    fullPath += '?' + stringifiedQuery;
+  }
+
+  let bodyJSON;
+  if (body) {
+    bodyJSON = JSON.stringify(body);
+  }
+
+  return fetch(fullPath, {
+    mode: 'no-cors',
+    method,
+    headers,
+    body: bodyJSON,
+  })
+    .then(parseBody)
+    .catch(e => console.log('error: ', e));
+};
+
+export const prettyUser = rawData => ({
+  id: rawData.id,
+  email: rawData.email_address,
+  firstName: rawData.merge_fields.FNAME,
+  lastName: rawData.merge_fields.LNAME,
+});
+
+const parseBody = json => JSON.parse(json._bodyInit);
+
+const getAuth = () => {
+  let authenticationString = Base64.btoa('florian.bienefelt:' + key);
+  authenticationString = 'Basic ' + authenticationString;
+  return authenticationString;
+};
+
+const headers = {
+  authorization: getAuth(),
+  Accept: 'application/json',
+  'Content-Type': 'application/json',
+};
--- api/contacts.test.js
+++
@@ -1,9 +0,0 @@
-import { getContacts } from './contacts';
-
-// describe('Contacts', () => {
-//   it('returns an array', () => {
-//     return getContacts()
-//       .then(result => console.log(result))
-//       .catch(e => console.log(e));
-//   });
-// });
